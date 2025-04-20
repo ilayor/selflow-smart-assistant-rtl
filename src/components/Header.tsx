0f1aa2b9e9a4c8fae6e4b12b66db0c3b261f4420
ilayor <ilay148@gmail.com>
2025-04-20 13:18:27 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,16 +27,17 @@ const Header: React.FC = () => {
         isScrolled ? 'py-2 bg-white shadow-md' : 'py-4 bg-transparent'
       }`}
     >
-<div className="flex justify-between items-center">
-  <div className="text-3xl font-bold">
-    <a href="/" className="inline-block">
-      <img src="/logo-small.png" alt="Selflow Logo" className="h-10" />
-    </a>
-  </div>
-</div>
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center">
 
-      
-          {/* Mobile menu button */}
+          {/* לוגו שמחזיר לעמוד הבית */}
+          <div className="text-3xl font-bold">
+            <a href="/" className="inline-block">
+              <img src="/logo-small.png" alt="Selflow Logo" className="h-10" />
+            </a>
+          </div>
+
+          {/* כפתור תפריט מובייל */}
           <button 
             className="md:hidden text-selflow-darkGreen focus:outline-none"
             onClick={toggleMenu}
@@ -44,8 +45,8 @@ const Header: React.FC = () => {
           >
             {isMenuOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
           </button>
-          
-          {/* Desktop navigation */}
+
+          {/* ניווט לדסקטופ */}
           <nav className="hidden md:flex space-x-8 space-x-reverse">
             <a 
               href="#features" 
@@ -79,8 +80,8 @@ const Header: React.FC = () => {
             </a>
           </nav>
         </div>
-        
-        {/* Mobile menu */}
+
+        {/* תפריט מובייל */}
         {isMenuOpen && (
           <div className="md:hidden absolute top-full left-0 right-0 bg-white shadow-lg py-4 animate-fade-in">
             <nav className="flex flex-col space-y-4 px-4">
